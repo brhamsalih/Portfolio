2fea49f9133ff71ff7247107796a1dc172e690ad
Ibrahim Salih Ismail <ibrahim.salih.f9@gmail.com>
2023-08-22 22:41:30 +0300

    add dataJson & deploy on railway postgresql

--- project/settings.py
+++ project/settings.py
@@ -74,8 +74,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-'''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
